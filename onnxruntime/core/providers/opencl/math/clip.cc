18f3378cd51adb91f9c5826256b30580a176603f
Guangyun Han <guangyunhan@microsoft.com>
2022-01-21 19:59:35 +0800

    fix Clip-12

--- onnxruntime/core/providers/opencl/math/clip.cc
+++ onnxruntime/core/providers/opencl/math/clip.cc
@@ -11,6 +11,27 @@ namespace {
 #include "opencl_generated/math/kernels/clip_image2d.cl.inc"
 }  // namespace
 
+Status ClipComputeImpl(const OpenCLExecutionProvider* exec, cl_kernel clip_kernel, const Tensor* X, const Tensor* Y, cl_float lower_bound, cl_float upper_bound) {
+  ZoneScopedN("ClipComputeImpl");
+  VLOG_CL_IMAGE2D("Input[0]", X);
+  VLOG_CL_IMAGE2D("Output[0]", Y);
+
+  const auto& X_shape = X->Shape();
+  auto desc = Image2DDesc::PackFromTensorNCHW(X_shape);
+  ZoneNamedN(_tracy_ClipNCHW, "Clip (kernel launch)", true);
+  ORT_RETURN_IF_ERROR(
+      KernelLauncher{clip_kernel}
+          .setArg<cl_int>(desc.Width())
+          .setArg<cl_int>(desc.Height())
+          .setImage2D(*X)
+          .setImage2D(*Y)
+          .setArg(lower_bound)
+          .setArg(upper_bound)
+          .Launch(*exec, desc.AsNDRange()));
+
+  return Status::OK();
+}
+
 class Clip6 : public OpenCLKernel {
  public:
   explicit Clip6(const OpKernelInfo& info)
@@ -23,33 +44,41 @@ class Clip6 : public OpenCLKernel {
   };
 
   Status Compute(OpKernelContext* context) const override {
-    ZoneScopedN("Clip6::Compute");
-    VLOG_CL_NODE();
+    VLOG_CL_NODE() << ", min:" << lower_bound_ << ", max:" << upper_bound_;
 
     const auto* X = context->Input<Tensor>(0);
     const auto* Y = context->Output(0, X->Shape());
-    VLOG_CL_IMAGE2D("Input[0]", X);
-    VLOG_CL_IMAGE2D("Output[0]", Y);
-
-    const auto& X_shape = X->Shape();
-    auto desc = Image2DDesc::PackFromTensorNCHW(X_shape);
-    ZoneNamedN(_tracy_ClipNCHW, "Clip (kernel launch)", true);
-    ORT_RETURN_IF_ERROR(
-        KernelLauncher{GetKernel("Clip")}
-            .setArg<cl_int>(desc.Width())
-            .setArg<cl_int>(desc.Height())
-            .setImage2D(*X)
-            .setImage2D(*Y)
-            .setArg(lower_bound_)
-            .setArg(upper_bound_)
-            .Launch(*exec_, desc.AsNDRange()));
-
-    return Status::OK();
+    return ClipComputeImpl(exec_, GetKernel("Clip"), X, Y, lower_bound_, upper_bound_);
   }
 
  private:
-  cl_float lower_bound_;
-  cl_float upper_bound_;
+  float lower_bound_;
+  float upper_bound_;
+};
+
+class Clip : public OpenCLKernel {
+ public:
+  explicit Clip(const OpKernelInfo& info)
+      : OpenCLKernel(info) {
+    VLOGS_DEFAULT(0) << "Init Clip (OpenCLKernel)";
+    LoadProgram(clip_kernel_src, clip_kernel_src_len);
+    LoadKernel("Clip");
+  }
+  Status Compute(OpKernelContext* context) const override {
+    VLOG_CL_NODE();
+    const auto* X = context->Input<Tensor>(0);
+    const auto* Y = context->Output(0, X->Shape());
+    float lower_bound = std::numeric_limits<float>::lowest();
+    float upper_bound = std::numeric_limits<float>::max();
+    if (context->InputCount() > 1) {
+      lower_bound = *(context->Input<Tensor>(1)->Data<float>());
+    }
+    if (context->InputCount() > 2) {
+      upper_bound = *(context->Input<Tensor>(2)->Data<float>());
+    }
+
+    return ClipComputeImpl(exec_, GetKernel("Clip"), X, Y, lower_bound, upper_bound);
+  }
 };
 
 ONNX_OPENCL_OPERATOR_KERNEL(
@@ -66,7 +95,8 @@ ONNX_OPENCL_OPERATOR_KERNEL(
     KernelDefBuilder()
         .TypeConstraint("T", DataTypeImpl::GetTensorType<float>())
         .InputMemoryType((OrtMemType)CLMemType::OPENCL_IMAGE_2D, 0)
+        .InputMemoryType(OrtMemTypeCPUInput, {1, 2})
         .OutputMemoryType((OrtMemType)CLMemType::OPENCL_IMAGE_2D, 0),
-    Clip6);
+    Clip);
 }  // namespace opencl
 }  // namespace onnxruntime
