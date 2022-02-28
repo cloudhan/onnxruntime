11f008a5002bbc14c909f408a2741066ad08158e
Guangyun Han <guangyunhan@microsoft.com>
2022-02-28 15:04:59 +0800

    backup cl build script

--- cl_host_dbg_build.sh
+++ cl_host_dbg_build.sh
@@ -5,10 +5,11 @@ set -ev
 ./build.sh \
     --cmake_generator Ninja \
     --config Debug \
-    --cmake_extra_defines CMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    --cmake_extra_defines CMAKE_EXPORT_COMPILE_COMMANDS=ON onnxruntime_ENABLE_RUNTIME_OPTIMIZATION_IN_MINIMAL_BUILD=ON\
     --use_opencl \
     --use_nnapi \
-    --minimal_build extended \
     --build_wheel \
-    --build_java \
+    --minimal_build disabled \
     --skip_submodule_sync --skip_tests \
+
+    # --build_java \
