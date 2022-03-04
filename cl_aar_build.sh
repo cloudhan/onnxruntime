92857e7f583794b385abcb042ed738e93db748d4
Guangyun Han <guangyunhan@microsoft.com>
2022-03-04 10:53:27 +0800

    backup cl_aar_build.sh

--- cl_aar_build.sh
+++ cl_aar_build.sh
@@ -5,4 +5,7 @@ set -ev
 export ANDROID_HOME=`realpath ~/android_sdk/`
 export ANDROID_NDK_HOME=`realpath ~/android_sdk/ndk/23.0.7599858`
 
-python tools/ci_build/github/android/build_aar_package.py tools/ci_build/github/android/cl_aar_build_settings.json
+python tools/ci_build/github/android/build_aar_package.py     \
+    --config Release                                          \
+    --build_dir build_aar                                     \
+    tools/ci_build/github/android/cl_aar_build_settings.json
